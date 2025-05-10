99279a57a2783fc58c5b65bef0af0ade3f65e632
Cholland14 <cmholland14@gmail.com>
2025-05-10 16:20:52 +1200

    initial commit

---
+++ Lab 4/Lab 4 - Mathematical Ops.js
@@ -0,0 +1,21 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(5,5)); //10
+console.log(subtract(9,3)); //6
+console.log(multiply(6,3)); // 18
+console.log(divide(12,4)); //3
---
+++ Lab 4/Lab 4 - Name.js
@@ -0,0 +1,11 @@
+function printHello(name) {
+    console.log('Hello ' + name)
+}
+
+printHello('Hagrid'); // Hello Hagrid
+
+//Concatenate
+let str1 = "Hello";
+let str2 = "World";
+let result = str1 + " " + str2;  // Adding a space between the words
+console.log(result);  // Output: Hello World
